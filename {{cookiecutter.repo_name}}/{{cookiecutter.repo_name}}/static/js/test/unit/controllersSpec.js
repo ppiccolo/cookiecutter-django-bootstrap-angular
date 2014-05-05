00f75c742d858b2bf739deab7cbd004ad3d04f4d
Maxime Moraine <maxime.moraine@gmail.com>
2014-05-05 18:07:53 +0200

    fix typo in angular test

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/static/js/test/unit/controllersSpec.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/static/js/test/unit/controllersSpec.js
@@ -5,7 +5,7 @@
 describe('controllers', function(){
   var scope;
 
-  beforeEach(module('test.controllers'));
+  beforeEach(module('{{cookiecutter.project_name}}.controllers'));
 
 
   it('should ....', inject(function($controller, $rootScope) {
